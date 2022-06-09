6ae3a6cefd5b54e31d8d344a96f7981b4122f28d
alvalenda <banysan@msn.com>
2022-06-09 13:05:55 -0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const prompt = require('prompt-sync')();
 /*
     Variáveis Globais
 */
-let playerName = "";
+
+let playerName;
 
 /*
     Funções
@@ -14,3 +16,31 @@ let playerName = "";
 /*
     Começa o Jogo
 */
+let salas = [];
+class Sala {
+    constructor(nome, salamae, inimigo) {
+        this.name = nome;
+        this.portas = { Mae: salamae, Filhas: [] };
+        this.inimigo = inimigo;
+
+        this.criaFilhas();
+    }
+
+    criaFilhas() {
+        for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
+            this.portas.Filhas.push('Fechada');
+        }
+    }
+}
+
+cela = new Sala('Cela da Prisão', false, "Rato Gigante");
+salas.push(cela);
+
+for (const sala of salas) {
+    for (const key in sala) {
+        if (Object.hasOwnProperty.call(sala, key)) {
+            const value = sala[key];
+            console.log(key, value);
+        }
+    }
+}
