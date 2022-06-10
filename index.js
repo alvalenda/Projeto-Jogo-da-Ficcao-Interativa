2e23089572133dda2b4db6cc1143c012ccd3f890
alvalenda <banysan@msn.com>
2022-06-10 09:37:54 -0300

    brincando com classes

--- index.js
+++ index.js
@@ -17,30 +17,56 @@ let playerName;
     Começa o Jogo
 */
 let salas = [];
+
 class Sala {
+    static num_salas = 0;
+
     constructor(nome, salamae, inimigo) {
         this.name = nome;
-        this.portas = { Mae: salamae, Filhas: [] };
+        this.portas = { Mae: salamae, Filha: [] };
         this.inimigo = inimigo;
 
-        this.criaFilhas();
+        this.criaSalaFilha();
     }
 
-    criaFilhas() {
+    criaSalaFilha() {
         for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
-            this.portas.Filhas.push('Fechada');
+            this.portas.Filha.push('Fechada');
+            Sala.num_salas += 1;
         }
     }
 }
 
-cela = new Sala('Cela da Prisão', false, "Rato Gigante");
+class Monstro {
+    static combates = [];
+
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+
+        Monstro.combates.push(this.nome);
+    }
+}
+
+const cela = new Sala('Cela da Prisão', false, 'Rato Gigante');
+let inimigo = new Monstro(`Rato Gigante`, 6, 3, 0);
+
 salas.push(cela);
 
-for (const sala of salas) {
-    for (const key in sala) {
-        if (Object.hasOwnProperty.call(sala, key)) {
-            const value = sala[key];
-            console.log(key, value);
+function imprimeObjeto(objeto) {
+    for (const chave in objeto) {
+        if (Object.hasOwnProperty.call(objeto, chave)) {
+            const valor = objeto[chave];
+            console.log(chave, valor);
         }
     }
 }
+
+for (const sala of salas) {
+    imprimeObjeto(sala);
+}
+imprimeObjeto(inimigo);
+
+console.log(Monstro.combates, Sala.num_salas);
