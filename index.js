f3279270baad224becefe71045f8b3170aada81d
alvalenda <banysan@msn.com>
2022-06-10 10:50:53 -0300

    testando herança e metodos staticos

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+console.clear();
 const prompt = require('prompt-sync')();
 /*
     Variáveis Globais
 */
 
-let playerName;
-
 /*
     Funções
 */
+function playerName() {
+    while (true) {
+        try {
+            const nome = prompt(`Qual é o seu nome sobrevivente 013? `);
+            if (nome.length < 3)
+                throw `Seu nome deve ter 3 ou mais caracteres...`;
+            return nome;
+        } catch (error) {}
+    }
+}
 
+function imprimeObjeto(objeto) {
+    for (const chave in objeto) {
+        if (Object.hasOwnProperty.call(objeto, chave)) {
+            const valor = objeto[chave];
+            console.log(chave, valor);
+        }
+    }
+}
 /*
     Trama
 */
@@ -16,10 +33,9 @@ let playerName;
 /*
     Começa o Jogo
 */
-let salas = [];
-
 class Sala {
     static num_salas = 0;
+    static salas = [];
 
     constructor(nome, salamae, inimigo) {
         this.name = nome;
@@ -27,12 +43,41 @@ class Sala {
         this.inimigo = inimigo;
 
         this.criaSalaFilha();
+        Sala.salas.push(this);
     }
 
     criaSalaFilha() {
         for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
             this.portas.Filha.push('Fechada');
             Sala.num_salas += 1;
+        }
+    }
+
+    static imprimeSalas() {
+        for (const sala of Sala.salas) {
+            imprimeObjeto(sala);
+        }
+    }
+}
+
+class Player {
+    constructor(nome, classe) {
+        this.nome = nome;
+        this.vida;
+        this.ataque;
+        this.defesa;
+        this.level = 1;
+
+        this.iniciaAtributos(classe);
+    }
+
+    iniciaAtributos(classe) {
+        if (classe === 'Fordo') {
+            [this.vida, this.ataque, this.defesa] = [8, 4, 3];
+        } else if (classe === 'Magro') {
+            [this.vida, this.ataque, this.defesa] = [6, 3, 5];
+        } else {
+            [this.vida, this.ataque, this.defesa] = [7, 4, 4];
         }
     }
 }
@@ -50,23 +95,23 @@ class Monstro {
     }
 }
 
-const cela = new Sala('Cela da Prisão', false, 'Rato Gigante');
-let inimigo = new Monstro(`Rato Gigante`, 6, 3, 0);
-
-salas.push(cela);
-
-function imprimeObjeto(objeto) {
-    for (const chave in objeto) {
-        if (Object.hasOwnProperty.call(objeto, chave)) {
-            const valor = objeto[chave];
-            console.log(chave, valor);
-        }
+class Ogro extends Monstro {
+    grita() {
+        console.log(
+            `${this.nome} forte!!! ${this.nome} esmaga ${player.nome}!!!`,
+        );
     }
 }
 
-for (const sala of salas) {
-    imprimeObjeto(sala);
-}
+const player = new Player(playerName(), 'Magro');
+const cela = new Sala('Cela da Prisão', false, 'Rato Gigante');
+let inimigo = new Monstro(`Rato Gigante`, 6, 3, 0);
+const ogro = new Ogro('Blop', 72, 11, 3);
+
 imprimeObjeto(inimigo);
+imprimeObjeto(Ogro);
+ogro.grita();
+imprimeObjeto(player);
+Sala.imprimeSalas();
 
 console.log(Monstro.combates, Sala.num_salas);
